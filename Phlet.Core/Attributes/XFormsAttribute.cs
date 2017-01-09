237f64f2e4fc18e7f8e77232f6d00ed46028ee8b
Steal <godhoop@nate.com>
2017-01-09 16:42:03 +0900

    First Commit

---
+++ Phlet.Core/Attributes/XFormsAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Phlet.Core.Attributes
+{
+    class XFormsAttribute : Attribute
+    {
+        public string Namespace { get; set; }
+        public string Name { get; set; }
+
+        public XFormsAttribute(string @namespace, string name)
+        {
+            this.Namespace = @namespace;
+            this.Name = name;
+        }
+
+        public XFormsAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Phlet/Interop/ExternDll.cs
@@ -0,0 +1,7 @@
+﻿namespace Phlet.Interop
+{
+    internal sealed class ExternDll
+    {
+        public const string User32 = "user32.dll";
+    }
+}
