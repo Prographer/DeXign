a8c7f6aa93163c6fb095e711798bd75bdd980152
Steal <godhoop@nate.com>
2017-02-02 00:00:53 +0900

    MenuItem InputGestureText 바인딩 오류 임시 해결

---
+++ DeXign/Controls/MenuItemEx.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DeXign.Controls
+{
+    // 왠지 모르게 InputGesutreText 바인딩이 이상하다.
+
+    [TemplatePart(Name = "PART_ShortCut", Type = typeof(ContentPresenter))]
+    public class MenuItemEx : MenuItem
+    {
+        ContentPresenter shortcut;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            shortcut = GetTemplateChild("PART_ShortCut") as ContentPresenter;
+            UpdateShortcut();
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            if (e.Property.Name == nameof(InputGestureText))
+            {
+                UpdateShortcut();
+            }
+        }
+
+        private void UpdateShortcut()
+        {
+            if (shortcut == null)
+                return;
+
+            if (string.IsNullOrEmpty(InputGestureText))
+            {
+                shortcut.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                shortcut.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ DeXign/MVVM/Converter/BaseValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace DeXign.Converter
+{
+    public abstract class BaseValueConverter<TFrom, TTo> : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert((TFrom)value, parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ConvertBack((TTo)value, parameter);
+        }
+
+        public abstract TTo Convert(TFrom value, object parameter);
+        public abstract TFrom ConvertBack(TTo value, object parameter);
+    }
+}
