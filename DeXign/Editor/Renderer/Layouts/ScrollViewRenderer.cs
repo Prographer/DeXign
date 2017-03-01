915610151c0eb54658a76b438a8b11c6c1039447
Steal <godhoop@nate.com>
2017-03-01 22:02:58 +0900

    왜인지모르게 추가된 Xamarin.Forms 패키지 제거

---
+++ DeXign/Editor/Renderer/Layouts/ScrollViewRenderer.cs
@@ -0,0 +1,17 @@
+﻿using DeXign.Core.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DeXign.Editor.Renderer
+{
+    class ScrollViewRenderer : LayerRenderer<PScrollView, ScrollViewer>
+    {
+        public ScrollViewRenderer(ScrollViewer adornedElement, PScrollView model) : base(adornedElement, model)
+        {
+        }
+    }
+}
