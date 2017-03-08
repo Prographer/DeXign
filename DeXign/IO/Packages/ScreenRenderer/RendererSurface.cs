e046beb710ae9aed693f85ffeb060281d8a03a14
Steal <godhoop@nate.com>
2017-03-09 01:27:10 +0900

    누락된 파일 복구

---
+++ DeXign/IO/Packages/ScreenRenderer/RendererSurface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DeXign.Editor.Renderer;
+
+namespace DeXign.IO
+{
+    public class RendererSurface
+    {
+        public Guid Guid { get; set; }
+        public RendererMetadata Metadata { get; set; }
+    }
+}
---
+++ DeXign/IO/Packages/ScreenRenderer/ScreenRendererPackageFile.cs
@@ -0,0 +1,38 @@
+﻿using DeXign.Core;
+using DeXign.Editor;
+using DeXign.Editor.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace DeXign.IO
+{ 
+    internal class ScreenRendererPackageFile : ModelPackageFile<Renderers>
+    {
+        public const string Path = "Renderers";
+
+        public ScreenRendererPackageFile(ScreenRenderer screenRenderer)
+        {
+            this.Name = $"{Path}\\{LayoutExtension.GetPageName(screenRenderer.Model)}.xml";
+
+            List<IRenderer> childrens = RendererTreeHelper
+                .FindChildrens<IRenderer>(screenRenderer)
+                .ToList();
+
+            childrens.Insert(0, screenRenderer);
+
+            this.Model = new Renderers()
+            {
+                Items = childrens
+                    .Select(c => new RendererSurface()
+                    {
+                        Guid = c.Model.Guid,
+                        Metadata = c.Metadata
+                    }).ToList()
+            };
+
+            base.CreateStream();
+        }
+    }
+}
